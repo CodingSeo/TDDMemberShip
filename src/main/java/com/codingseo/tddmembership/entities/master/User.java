f34066d057806095b80f8c73131d421f75469c7d
CodingSeo <seojingyo@hotmail.com>
2021-12-22 17:27:41 +0900

    Entity Column 값 추가

--- src/main/java/com/codingseo/tddmembership/entities/master/User.java
+++ src/main/java/com/codingseo/tddmembership/entities/master/User.java
@@ -3,6 +3,7 @@ package com.codingseo.tddmembership.entities.master;
 import com.codingseo.tddmembership.entities.BaseEntity;
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
@@ -11,6 +12,9 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
+    @Column(nullable = false, length = 25)
     private String name;
+
+    @Column(nullable = false)
     private Integer pass;
 }
--- src/main/java/com/codingseo/tddmembership/entities/sub/MemberShip.java
+++ src/main/java/com/codingseo/tddmembership/entities/sub/MemberShip.java
@@ -2,7 +2,9 @@ package com.codingseo.tddmembership.entities.sub;
 
 import com.codingseo.tddmembership.entities.BaseEntity;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
@@ -11,6 +13,10 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberShip extends BaseEntity {
+    @Column(nullable = false, length = 25)
     private String membershipName;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
     private Integer point;
 }
