6d883ea278967e2b1cef611d7ef51400b9654058
CodingSeo <seojingyo@hotmail.com>
2021-12-22 18:12:06 +0900

    parameter final 추가

--- src/main/java/com/codingseo/tddmembership/repositories/master/UserRepository.java
+++ src/main/java/com/codingseo/tddmembership/repositories/master/UserRepository.java
@@ -4,5 +4,5 @@ import com.codingseo.tddmembership.entities.master.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByName(String userName);
+    User findByName(final String userName);
 }
